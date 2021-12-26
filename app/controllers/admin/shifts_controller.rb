a5e94404aa8aafbff45c1537f6c94cc2dd374cbd
akaishisho <90311696+akaishisho@users.noreply.github.com>
2021-12-26 15:48:37 +0000

    [Update]Excel_export/partial(shifts)/user_shifts

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -3,7 +3,15 @@ class Admin::ShiftsController < ApplicationController
 
   def index
     @shifts = Shift.all
-
+    # --ここから--(メンターさん追記)
+    @data = {}
+    @shifts.each do |shift|
+      if @data[shift.date.to_s.to_sym] == nil
+        @data[shift.date.to_s.to_sym] = []
+      end
+      @data[shift.date.to_s.to_sym] << shift
+    end
+    # --ここまで--
     respond_to do |format|
       format.html
       format.xlsx do
